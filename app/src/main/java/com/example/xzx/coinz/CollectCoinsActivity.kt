218d3b98b569204af80a3e4ee66090abc1499173
xzxedu <s1787273@ed.ac.uk>
2018-12-11 22:15:54 +0000

    wallet activity

--- app/src/main/java/com/example/xzx/coinz/CollectCoinsActivity.kt
+++ app/src/main/java/com/example/xzx/coinz/CollectCoinsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.xzx.coinz
 
+import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
@@ -10,12 +11,15 @@ import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import com.firebase.ui.auth.R.id.email
 import com.firebase.ui.auth.R.id.password
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
 import com.google.gson.JsonObject
@@ -33,9 +37,13 @@ class CollectCoinsActivity: AppCompatActivity(){
     private val currentUserDocRef: DocumentReference
         get() = firestore.document("users/${FirebaseAuth.getInstance().currentUser?.uid
                 ?: throw NullPointerException("UID is null.")}")
-
-
     private val DOCUMENT_KEY= currentUserDocRef.id
+    private var DocumentData= mutableMapOf<String, Any?>()
+    private var DolrNum:Float = 0f
+    private var QuidNum:Float = 0f
+    private var ShilNum:Float = 0f
+    private var PenyNum:Float = 0f
+
     companion object
     {
         private const val TAG = "CollectCoinsActivity"
@@ -45,7 +53,6 @@ class CollectCoinsActivity: AppCompatActivity(){
         private val SHIL_FIELD = "SHIL"
         private val PENY_FIELD = "PENY"
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,10 +64,9 @@ class CollectCoinsActivity: AppCompatActivity(){
                 .build()
         firestore?.firestoreSettings = settings
         firestoreCoinz = firestore?.collection(COLLECTION_KEY)?.document(DOCUMENT_KEY)
-//        realtimeUpdateListener()
-
+        getCurrentUser(currentUserDocRef)
         // check location service permission
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest
+        if (ActivityCompat.checkSelfPermission(this, Manifest
                         .permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
             return
 
@@ -98,20 +104,43 @@ class CollectCoinsActivity: AppCompatActivity(){
         }
     }
 
+    fun getCurrentUser(currentUserDocRef: DocumentReference) {
+        currentUserDocRef.get().addOnCompleteListener(OnCompleteListener<DocumentSnapshot> { task ->
+            if (task.isSuccessful) {
+                val document = task.result
+                if (document != null) {
+                    DocumentData = document.data!!
+                    Log.d("wallet", "DocumentSnapshot data: " + DocumentData)
+                    DolrNum = DocumentData.get("DOLR").toString().toFloat()
+                    ShilNum = DocumentData.get("SHIL").toString().toFloat()
+                    PenyNum = DocumentData.get("PENY").toString().toFloat()
+                    QuidNum = DocumentData.get("QUID").toString().toFloat()
+                } else {
+                    toast("No such document")
+                }
+            } else {
+                Log.d("wallet", "get failed with ", task.exception)
+            }
+        })
+
+    }
+
     private fun updateCoinsFirestore(f: Feature) {
         val j = f.properties() as JsonObject
         var currency :Map<String,String> ?= null
+        var acquireValue = j.get("value").toString().replace('\"',' ').trim().toFloat()
         when(j.get("currency").toString().replace('\"',' ').trim()){
-            "DOLR" ->{ currency = mapOf(DOLR_FIELD to j.get("value").toString().replace('\"',' ').trim())}
-            "SHIL" ->{ currency = mapOf(SHIL_FIELD to j.get("value").toString().replace('\"',' ').trim())}
-            "PENY" ->{ currency= mapOf(PENY_FIELD to j.get("value").toString().replace('\"',' ').trim())}
-            "QUID" ->{ currency= mapOf(QUID_FIELD to j.get("value").toString().replace('\"',' ').trim())}
+            "DOLR" ->{ currency = mapOf(DOLR_FIELD to (acquireValue.plus(DolrNum)).toString())}
+            "SHIL" ->{ currency = mapOf(SHIL_FIELD to (acquireValue.plus(ShilNum)).toString())}
+            "PENY" ->{ currency = mapOf(PENY_FIELD to  (acquireValue.plus(PenyNum)).toString())}
+            "QUID" ->{ currency = mapOf(QUID_FIELD to  (acquireValue.plus(QuidNum)).toString())}
         }
-     // send the coins and listen for success or failure
+        // send the coins and listen for success or failure
         if (currency !=  null){
             firestoreCoinz!!.update(currency)
-                .addOnSuccessListener { toast("Collect Coins Successfully!") } // anko
-                .addOnFailureListener { e -> Log.e(TAG, e.message) }
+                    .addOnSuccessListener { toast("Collect Coins Successfully!") } // anko
+                    .addOnFailureListener { e -> Log.e(TAG, e.message) }
         }
     }
+
 }
